f8dd356939d9f0e38163dfc16ec35106aff1992b
hop100 <mohab.esam2020@gmail.com>
2026-09-26 00:58:00 +0300

    Update chain_poops.js

--- slopkit/chain_poops.js
+++ slopkit/chain_poops.js
@@ -455,7 +455,7 @@ let payloadRunning = false;
                 mark("ALREADY-ROOT", "getuid=" + uid0 + " setuid(0)=" + su0);
                 var m = document.getElementById("msgs");
                 if (m) {
-                    m.innerHTML ='<span style="color: #10e610;">GoldHEN Sudah Dimuat ...</span>';
+                    m.innerHTML ='<span style="color: #10e610;">ENG/MOHAB ESAM .</span>';
                 }
                 return;
             }
